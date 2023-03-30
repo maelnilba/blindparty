5edd35a2c78b8ed3fa2e6eb30278b4aba9d29bfd
Marie Rioux <mael.rioux@axopen.com>
2023-03-30 16:55:55 +0200

    Only if running

--- src/pages/api/prpc/[prpc].ts
+++ src/pages/api/prpc/[prpc].ts
@@ -8,8 +8,8 @@ import { env } from "../../../env/server.mjs";
 const webhooks = createNextWehbookApiHandler<typeof prpc>({
   existence: async (data, ctx) => {
     if (data.name === "channel_vacated") {
-      await ctx.prisma.party.update({
-        where: { id: data.channel.id },
+      await ctx.prisma.party.updateMany({
+        where: { id: data.channel.id, status: "RUNNING" },
         data: {
           status: "CANCELED",
         },
