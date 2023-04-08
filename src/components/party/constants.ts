fa5cabb083817b05abd32856eb15497bb9abd808
Marie Rioux <mael.rioux@axopen.com>
2023-04-08 21:42:20 +0200

    Reset back timers constant, oopsi ^^

--- src/components/party/constants.ts
+++ src/components/party/constants.ts
@@ -1,5 +1,14 @@
 import { ONE_SECOND_IN_MS } from "lib/helpers/date";
 
-export const VIEW_SCORE_MS = 2 * ONE_SECOND_IN_MS;
-export const GUESS_MS = 2 * ONE_SECOND_IN_MS;
-export const TRACK_TIMER_MS = 2 * ONE_SECOND_IN_MS;
+export const VIEW_SCORE_MS =
+  process.env.NODE_ENV === "production"
+    ? 10 * ONE_SECOND_IN_MS
+    : 10 * ONE_SECOND_IN_MS;
+export const GUESS_MS =
+  process.env.NODE_ENV === "production"
+    ? 30 * ONE_SECOND_IN_MS
+    : 30 * ONE_SECOND_IN_MS;
+export const TRACK_TIMER_MS =
+  process.env.NODE_ENV === "production"
+    ? 5 * ONE_SECOND_IN_MS
+    : 5 * ONE_SECOND_IN_MS;
