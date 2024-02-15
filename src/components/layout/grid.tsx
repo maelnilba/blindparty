655ad5a9507df860aaaeb8d6c5aa39916f1e4cd0
Marie nilba <marie.rioux@axopen.com>
2024-02-15 19:05:25 +0100

    Style rework #120 Added Grid

--- src/components/layout/grid.tsx
+++ src/components/layout/grid.tsx
@@ -1,7 +1,12 @@
 export const Grid = () => {
   return (
-    <div className="absolute top-0 flex h-[40rem] w-full transform items-center justify-center bg-black bg-grid-gray-900/[1]">
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:linear-gradient(to_bottom,transparent_20%,black)]"></div>
-    </div>
+    <>
+      <div className="absolute top-0 flex h-[12rem] w-full transform items-center justify-center bg-black bg-dot-gray-900/[1]">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:linear-gradient(to_bottom,transparent_20%,black)]"></div>
+      </div>
+      <div className="absolute bottom-0 flex h-[12rem] w-full transform items-center justify-center bg-black bg-dot-gray-900/[1]">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:linear-gradient(to_top,transparent_20%,black)]"></div>
+      </div>
+    </>
   );
 };
