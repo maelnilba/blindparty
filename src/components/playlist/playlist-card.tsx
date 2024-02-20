0229006641b419f27acb05b05dea5ec1d36e9447
Marie nilba <marie.rioux@axopen.com>
2024-02-20 20:27:59 +0100

    #116

--- src/components/playlist/playlist-card.tsx
+++ src/components/playlist/playlist-card.tsx
@@ -39,18 +39,24 @@ export const PlaylistCard = ({ playlist, onDelete }: PlaylistCardProps) => {
           Modifier
         </Link>
 
-        <ConfirmationModal
-          title={`Supprimer la playlist`}
-          message={`Êtes vous certain de vouloir supprimer votre playlist ${playlist.name} ? Cette action est irreversible.`}
-          actions={["Supprimer"]}
-          onSuccess={() => {
-            onDelete(playlist);
-          }}
-        >
-          <button className="rounded-full bg-pink-200 px-6 py-1 text-lg font-semibold text-black no-underline transition-transform hover:scale-105">
+        <ConfirmationModal.Root>
+          <ConfirmationModal.Trigger className="rounded-full bg-pink-200 px-6 py-1 text-lg font-semibold text-black no-underline transition-transform hover:scale-105">
             Supprimer
-          </button>
-        </ConfirmationModal>
+          </ConfirmationModal.Trigger>
+          <ConfirmationModal.Title>
+            Supprimer la playlist{" "}
+          </ConfirmationModal.Title>
+          <ConfirmationModal.Message>
+            Êtes vous certain de vouloir supprimer votre playlist{" "}
+            {playlist.name} ? Cette action est irreversible.
+          </ConfirmationModal.Message>
+          <ConfirmationModal.Action
+            className="rounded-full bg-white px-6 py-1 text-center text-lg font-semibold text-black no-underline transition-transform hover:scale-105"
+            onClick={() => onDelete(playlist)}
+          >
+            Supprimer
+          </ConfirmationModal.Action>
+        </ConfirmationModal.Root>
       </div>
     </div>
   );
